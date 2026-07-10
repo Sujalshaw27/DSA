429c88226466c37ea5da51331bdb402e7de3c26e
Sujal shaw <shawsujal279@gmail.com>
2026-07-10 19:17:35 +0530

    Time: 14 ms (31.77%), Space: 46.2 MB (31.9%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -4,15 +4,15 @@ class Solution {
         s = s.toLowerCase();
         s = s.replaceAll("[^a-z0-9]", "");
         int i =0;
-       
+        int n = s.length();
         int j = s.length()-1;
         
-        while(i<j){
+        for(i = 0; i<n/2; i++){
             if(s.charAt(i)!=s.charAt(j)){
                 return false;
             }
-            i++;
             j--;
+            
         }
         return true;     
     }
